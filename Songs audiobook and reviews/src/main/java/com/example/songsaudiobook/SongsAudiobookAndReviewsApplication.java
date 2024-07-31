cdeed95b059f0cdbe69620631e6728b3781ebf31
Satyam Kahali <139269813+SATYAMKAHALI@users.noreply.github.com>
2024-07-31 22:39:10 +0530

    Add files via upload

---
+++ Songs audiobook and reviews/src/main/java/com/example/songsaudiobook/SongsAudiobookAndReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.songsaudiobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsAudiobookAndReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongsAudiobookAndReviewsApplication.class, args);
+    }
+
+}
